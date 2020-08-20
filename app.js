2cf3a4f50b4809a584b9e17aceed8372b01fd427
Abel Shedrack Nicholas <shedrackejeh458@gmail.com>
2020-08-20 20:02:15 +0100

    new folder

---
+++ app.js
@@ -0,0 +1,29 @@
+function add(num1, num2){
+    let a = num1 + num2;
+    return a;
+}
+
+function subtract(num1, num2){
+    let b = num1 - num2;
+    return b;
+}
+
+function divide(num1, num2){
+    let c = num1 / num2;
+    return c;
+}
+
+function multiply(num1, num2){
+    let d = num1 * num2;
+    return d;
+}
+
+let answer1 = add(10, 5);
+let answer2 = subtract(10, 5);
+let answer3 = divide(10, 2);
+let answer4 = multiply(10, 4);
+
+console.log(answer1);
+console.log(answer2);
+console.log(answer3);
+console.log(answer4);
